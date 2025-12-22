9432373814578e7b267f54efca98dfcfbabada70
alitolibov <alitolibov066@gmail.com>
2025-12-22 16:12:15 +0500

    feat: setup drizzle migrations and recreate database schema

--- back/drizzle/0000_eager_deadpool.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"username" varchar(256) NOT NULL,
-	"password" varchar(100) NOT NULL,
-	"bio" varchar(255),
-	"avatar_id" integer,
-	"created_at" timestamp DEFAULT now(),
-	CONSTRAINT "users_username_unique" UNIQUE("username")
-);
